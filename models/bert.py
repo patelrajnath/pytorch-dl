71afbfa08aecfb3de8f06471d40cf252c287a213
Raj Nath Patel <patelrajnath@gmail.com>
2020-01-14 23:07:36 +0000

    Bert Model.

---
+++ models/bert.py
@@ -0,0 +1,27 @@
+from torch import nn
+
+from models.embeddings.bert_embeddings import BertEmbeddings
+from models.transformer import TransformerBlock
+
+
+class Bert(nn.Module):
+    """
+    """
+    def __init__(self, vocab_size, width=512, depth=12 ,heads=8):
+        super().__init__()
+        self.h = width
+        self.d = depth
+        self.heads = heads
+        self.vocab_size = vocab_size
+
+        self.bert_embeddings = BertEmbeddings(vocab_size, self.h)
+
+        tblocks = []
+        for i in range(self.d):
+            tblocks.append(TransformerBlock(k=self.h, heads=heads))
+        self.tblocks = nn.Sequential(*tblocks)
+
+    def forward(self, x, segment_label):
+        x = self.bert_embeddings(x, segment_label)
+        x = self.tblocks(x)
+        return x
