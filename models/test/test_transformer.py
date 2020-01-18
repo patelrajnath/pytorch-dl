30a5c65a90844dbe2ad17f7d64cd6b1324fb2b81
Raj Nath Patel <patelrajnath@gmail.com>
2020-01-18 16:42:00 +0000

    Add msaked LM trainer fro encoder-decoder model.

--- models/test/test_transformer.py
+++ models/test/test_transformer.py
@@ -52,9 +52,23 @@ b=16
 x = np.random.rand(b, w, k)
 
 model = TransformerEncoderDecoder(k, h, depth=2, num_emb=vocab_size, num_emb_target=vocab_size, max_len=80)
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-for _ in range(10):
+criterion = nn.NLLLoss(ignore_index=0)
+optimizer = Adam(lr=0.0001, params=model.parameters())
+lr_schedular = lr_scheduler.LambdaLR(optimizer, lambda i: min(i / (lr_warmup / batch_size), 1.0))
+
+cuda_condition = torch.cuda.is_available()
+device = torch.device("cuda:0" if cuda_condition else "cpu")
+
+if cuda_condition:
+    model.cuda()
+
+if cuda_condition and torch.cuda.device_count() > 1:
+    print("Using %d GPUS for BERT" % torch.cuda.device_count())
+    model = nn.DataParallel(model, device_ids=[0,1,2,3])
+
+
+for _ in range(100):
     avg_loss = 0
     # Setting the tqdm progress bar
     data_iter = tqdm.tqdm(enumerate(data_loader),
@@ -63,4 +77,14 @@ for _ in range(10):
     for i, data in data_iter:
         data = {key: value.to(device) for key, value in data.items()}
         bert_input, bert_label, segment_label, is_next = data
-        model(data[bert_input])
+        bert_out = model(data[bert_input])
+
+        loss = criterion(bert_out.transpose(1, 2), data[bert_label])
+        # next_loss = criterion(sentence_pred, data[is_next])
+        # loss = next_loss + mask_loss
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        lr_schedular.step()
+        avg_loss += loss.item()
+    print(avg_loss)
