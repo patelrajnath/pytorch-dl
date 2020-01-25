0908610c2205a490e31ae617cb179fe1c9caaee9
Raj Nath Patel <patelrajnath@gmail.com>
2020-01-25 15:13:45 +0000

    Add code to save check_points.

--- models/utils/model_utils.py
+++ models/utils/model_utils.py
@@ -5,6 +5,10 @@ pytorch-dl
 Created by raj at 11:05
 Date: January 18, 2020
 """
+import logging
+import os
+import traceback
+
 import torch
 
 
@@ -32,4 +36,52 @@ def d(tensor=None):
     if tensor is None:
         return 'cuda' if torch.cuda.is_available() else 'cpu'
     return 'cuda' if tensor.is_cuda else 'cpu'
+
+
+def torch_persistent_save(*args, **kwargs):
+    for i in range(3):
+        try:
+            return torch.save(*args, **kwargs)
+        except Exception:
+            if i == 2:
+                logging.error(traceback.format_exc())
+
+
+def save_state(filename, model, criterion, optimizer,
+               num_updates, optim_history=None, extra_state=None):
+    if optim_history is None:
+        optim_history = []
+    if extra_state is None:
+        extra_state = {}
+    print("Saving checkpoint at-", filename)
+    state_dict = {
+        'model': model.state_dict(),
+        'num_updates': num_updates,
+        'optimizer_history': optim_history + [
+            {
+                'criterion_name': criterion.__class__.__name__,
+                'optimizer_name': optimizer.__class__.__name__,
+            }
+        ],
+        'extra_state': extra_state,
+    }
+    torch_persistent_save(state_dict, filename)
+
+
+def load_model_state(filename, model, cuda_device=None):
+    if not os.path.exists(filename):
+        print("Starting training from scratch.")
+        return 0
+
+    print("Loading model from checkpoints", filename)
+    state = torch.load(filename)
+
+    # load model parameters
+    try:
+        model.load_state_dict(state['model'])
+    except Exception:
+        raise Exception('Cannot load model parameters from checkpoint, '
+                        'please ensure that the architectures match')
+
+    return state['num_updates']
 
