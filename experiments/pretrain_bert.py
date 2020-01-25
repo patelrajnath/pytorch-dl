1953887895c8a8c3dec6e3e421a099d9a7933b13
Raj Nath Patel <patelrajnath@gmail.com>
2020-01-25 21:26:58 +0000

    Add command-line options for model parameters.

--- experiments/pretrain_bert.py
+++ experiments/pretrain_bert.py
@@ -6,6 +6,7 @@ Created by raj at 11:05
 Date: January 18, 2020	
 """
 import sys
+from argparse import ArgumentParser
 
 import torch
 import tqdm
@@ -18,41 +19,40 @@ from dataset.vocab import WordVocab
 from models.bert import Bert
 from models.bert_lm import BertLanguageModel
 
-input_file= sys.argv[1]
-with open(input_file) as f:
-    vocab = WordVocab(f)
-    vocab.save_vocab("experiments/sample-data/vocab.pkl")
 
-vocab = WordVocab.load_vocab("experiments/sample-data/vocab.pkl")
-data_set = BertDataSet(input_file, vocab, max_size=512)
+def go(arg):
+    with open(arg.path) as f:
+        vocab = WordVocab(f)
+        vocab.save_vocab("experiments/sample-data/vocab.pkl")
 
-lr_warmup = 1000
-batch_size = 4
+    vocab = WordVocab.load_vocab("experiments/sample-data/vocab.pkl")
+    data_set = BertDataSet(arg.path, vocab, arg.max_length)
 
-data_loader = DataLoader(data_set, batch_size=4)
+    lr_warmup = arg.lr_warmup
+    batch_size = arg.batch_size
 
-vocab_size = len(vocab.stoi)
-bert = Bert(vocab_size)
-model = BertLanguageModel(bert, vocab_size)
+    data_loader = DataLoader(data_set, batch_size=batch_size)
 
-criterion = nn.NLLLoss(ignore_index=0)
-optimizer = Adam(lr=0.0001, params=model.parameters())
-lr_schedular = lr_scheduler.LambdaLR(optimizer, lambda i: min(i / (lr_warmup / batch_size), 1.0))
+    vocab_size = len(vocab.stoi)
+    bert = Bert(vocab_size, width=arg.embedding_size, depth=arg.depth, heads=arg.num_heads)
+    model = BertLanguageModel(bert, vocab_size)
 
-cuda_condition = torch.cuda.is_available()
-device = torch.device("cuda:0" if cuda_condition else "cpu")
+    criterion = nn.NLLLoss(ignore_index=0)
+    optimizer = Adam(lr=arg.lr, params=model.parameters())
+    lr_schedular = lr_scheduler.LambdaLR(optimizer, lambda i: min(i / (lr_warmup / batch_size), 1.0))
 
-if cuda_condition:
-    model.cuda()
+    cuda_condition = torch.cuda.is_available()
+    device = torch.device("cuda:0" if cuda_condition else "cpu")
 
-if cuda_condition and torch.cuda.device_count() > 1:
-    print("Using %d GPUS for BERT" % torch.cuda.device_count())
-    model = nn.DataParallel(model, device_ids=[0,1,2,3])
-    # model = nn.parallel.DistributedDataParallel(model, device_ids=[0,1,2,3])
+    if cuda_condition:
+        model.cuda()
 
+    if cuda_condition and torch.cuda.device_count() > 1:
+        print("Using %d GPUS for BERT" % torch.cuda.device_count())
+        model = nn.DataParallel(model, device_ids=[0,1,2,3])
+        # model = nn.parallel.DistributedDataParallel(model, device_ids=[0,1,2,3])
 
-def train():
-    for epoch in range(100):
+    for epoch in range(arg.num_epochs):
         avg_loss = 0
         # Setting the tqdm progress bar
         data_iter = tqdm.tqdm(enumerate(data_loader),
@@ -74,8 +74,8 @@ def train():
         print('Average loss: {}'.format(avg_loss / len(data_iter)))
 
 
-if __name__ == '__main__':
-    train()
+# if __name__ == '__main__':
+#     train()
     # num_processes = 4
     # NOTE: this is required for the ``fork`` method to work
     # model.share_memory()
@@ -87,3 +87,74 @@ if __name__ == '__main__':
     #     processes.append(p)
     # for p in processes:
     #     p.join()
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+
+    parser.add_argument("-e", "--num-epochs",
+                        dest="num_epochs",
+                        help="Number of epochs.",
+                        default=80, type=int)
+
+    parser.add_argument("-b", "--batch-size",
+                        dest="batch_size",
+                        help="The batch size.",
+                        default=4, type=int)
+
+    parser.add_argument("-l", "--learn-rate",
+                        dest="lr",
+                        help="Learning rate",
+                        default=0.0001, type=float)
+
+    parser.add_argument("-P", "--path", dest="path",
+                        help="Tensorboard logging directory",
+                        default='experiments/sample-data/bert-example.txt')
+
+    parser.add_argument("-f", "--final", dest="final",
+                        help="Whether to run on the real test set (if not included, the validation set is used).",
+                        action="store_true")
+
+    parser.add_argument("--max-pool", dest="max_pool",
+                        help="Use max pooling in the final classification layer.",
+                        action="store_true")
+
+    parser.add_argument("-E", "--embedding", dest="embedding_size",
+                        help="Size of the character embeddings.",
+                        default=128, type=int)
+
+    parser.add_argument("-V", "--vocab-size", dest="vocab_size",
+                        help="Number of words in the vocabulary.",
+                        default=50_000, type=int)
+
+    parser.add_argument("-M", "--max", dest="max_length",
+                        help="Max sequence length. Longer sequences are clipped (-1 for no limit).",
+                        default=512, type=int)
+
+    parser.add_argument("-H", "--heads", dest="num_heads",
+                        help="Number of attention heads.",
+                        default=8, type=int)
+
+    parser.add_argument("-d", "--depth", dest="depth",
+                        help="Depth of the network (nr. of self-attention layers)",
+                        default=6, type=int)
+
+    parser.add_argument("-r", "--random-seed",
+                        dest="seed",
+                        help="RNG seed. Negative for random",
+                        default=1, type=int)
+
+    parser.add_argument("--lr-warmup",
+                        dest="lr_warmup",
+                        help="Learning rate warmup.",
+                        default=10_000, type=int)
+
+    parser.add_argument("--gradient-clipping",
+                        dest="gradient_clipping",
+                        help="Gradient clipping.",
+                        default=1.0, type=float)
+
+    options = parser.parse_args()
+
+    print('OPTIONS ', options)
+
+    go(options)
