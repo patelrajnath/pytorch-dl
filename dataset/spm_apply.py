b1ae02f8feb28486d3b16ada4dc5c29c6adb111b
Raj Nath Patel <patelrajnath@gmail.com>
2020-01-25 11:54:01 +0000

    Add command line input.

--- dataset/spm_apply.py
+++ dataset/spm_apply.py
@@ -5,17 +5,18 @@ pytorch-dl
 Created by raj at 09:59 
 Date: January 25, 2020	
 """
+import sys
 
 import sentencepiece as sp
 
 spm = sp.SentencePieceProcessor()
 spm.Load('m.model')
-with open('experiments/sample-data/europarl.it') as inp, \
-        open('experiments/sample-data/europarl.it.enc', 'w') as out:
+input_file = sys.argv[1]
+with open(input_file) as inp, \
+        open(input_file + '.enc', 'w') as out:
     for line in inp:
         encoded = spm.encode_as_pieces(line)
         out.write(' '.join(encoded) + '\n')
-
 
 # text_ids = spm.encode_as_ids("Only then can we speak of 'communication'.")
 # print(text_ids)
