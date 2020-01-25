667d844836b0af66d4ebd5537c53aee28124b55d
Raj Nath Patel <patelrajnath@gmail.com>
2020-01-25 11:18:41 +0000

    Add module to load mbert training data.

---
+++ dataset/data_loader_mbert.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+pytorch-dl
+Created by raj at 10:30 
+Date: January 25, 2020	
+"""
+
+import random
+
+import torch
+import tqdm
+from torch.utils.data import Dataset
+
+
+class MBertDataSet(Dataset):
+    """
+    """
+    def __init__(self, corpus_path, vocab, max_size, corpus_lines=None, encoding="utf-8", on_memory=True):
+        self.corpus_path = corpus_path
+        self.corpus_lines = corpus_lines
+        self.vocab = vocab
+        self.max_size = max_size
+
+        with open(self.corpus_path, "r", encoding=encoding) as f:
+            self.lines = [line[:-1].strip()
+                          for line in tqdm.tqdm(f, desc="Loading Dataset", total=self.corpus_lines)]
+            self.corpus_lines = len(self.lines)
+
+    def __len__(self):
+        return self.corpus_lines
+
+    def __getitem__(self, idx):
+        line = self.get_corpus_line(idx)
+        t1_tokens, t1_label = self.random_words(line)
+
+        t1_random = [self.vocab.sos_index] + t1_tokens + [self.vocab.eos_index]
+
+        t1_label = [self.vocab.pad_index] + t1_label + [self.vocab.pad_index]
+
+        bert_input = t1_random[:self.max_size]
+        bert_label = t1_label[:self.max_size]
+
+        padding = [self.vocab.pad_index for _ in range(self.max_size - len(bert_input))]
+        bert_input.extend(padding), bert_label.extend(padding)
+
+        output = {
+            "bert_input": bert_input,
+            "bert_label": bert_label
+        }
+
+        return {key: torch.tensor(value) for key, value in output.items()}
+
+    def random_words(self, sentence):
+        tokens = sentence.split()
+        output_labels = list()
+        for i, token in enumerate(tokens):
+            prob = random.random()
+            if prob < 0.35:
+                prob /= 0.35
+                # 80% of the tokens we replace with mask
+                if prob < 0.80:
+                    tokens[i] = self.vocab.mask_index
+                # 10% of tokens to be replaced with random word
+                elif prob < 0.90:
+                    tokens[i] = random.randrange(len(self.vocab.stoi))
+                # Remaining 10% we keep actual word
+                else:
+                    tokens[i] = self.vocab.stoi.get(token, self.vocab.unk_index)
+            else:
+                tokens[i] = self.vocab.stoi.get(token, self.vocab.unk_index)
+
+            # add correct tokens to be predicted during training
+            output_labels.append(self.vocab.stoi.get(token, self.vocab.unk_index))
+        return tokens, output_labels
+
+    def get_corpus_line(self, index):
+        return self.lines[index]
