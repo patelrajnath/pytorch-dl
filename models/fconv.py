8fed1e87da9f4f7fc7cf60f0c271324506b618dc
Raj Nath Patel <patelrajnath@gmail.com>
2020-02-09 12:41:32 +0000

    Add torachvision for CNN training.

--- models/fconv.py
+++ models/fconv.py
@@ -6,6 +6,7 @@ Created by raj at 10:20
 Date: February 09, 2020	
 """
 import torch
+from spacy.cli import train
 from torch import nn
 import torch.nn.functional as F
 import torchvision
@@ -21,8 +22,7 @@ class CNN(nn.Module):
         self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size)
         self.conv2 = nn.Conv2d(in_channels=out_channels, out_channels=2 * out_channels, kernel_size=kernel_size)
 
-        # Compute output dimension after convolution/max-pooling, Oh = (n -f - 2p)/s + 1
-
+        # Compute output dimension after convolution/max-pooling (4 * 4 in self.fc1), Oh = (n -f - 2p)/s + 1
         self.fc1 = nn.Linear(2 * out_channels * 4 * 4, 120)
         self.fc2 = nn.Linear(120, 60)
         self.out = nn.Linear(60, 10)
@@ -63,14 +63,24 @@ train_data = FashionMNIST(
          ])
 )
 
+valid_data = FashionMNIST(
+    root="../data/FashionMNIST",
+    download=True,
+    train=False,
+    transform=torchvision.transforms.Compose(
+        [torchvision.transforms.ToTensor()
+         ])
+)
+
 # image, label = next(iter(train_data))
-train_loader = DataLoader(train_data, batch_size=100, shuffle=True)
+train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
+validation_loader = DataLoader(valid_data, batch_size=32)
 cnn = CNN()
 optimizer = Adam(params=cnn.parameters(), lr=0.01)
 
 for i in range(10):
     total_loss = 0
-    total_correct = 0
+    total_correct = 0.0
     for batch in train_loader:
         images, labels = batch
 
@@ -84,3 +94,13 @@ for i in range(10):
         total_loss += loss.item()
         total_correct += get_num_correct(pred, labels)
     print(total_loss, total_correct)
+    print(total_correct / len(train_data))
+    with torch.no_grad():
+        total_correct = .0
+        for batch in validation_loader:
+            images, labels = batch
+            pred = cnn(images)
+            total_correct += get_num_correct(pred, labels)
+        print(total_correct)
+        print(total_correct/len(valid_data))
+
