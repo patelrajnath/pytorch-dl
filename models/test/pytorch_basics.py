7945909425ceb96ba1bae6038db96440cdba2ad2
Raj Nath Patel <patelrajnath@gmail.com>
2020-02-10 17:11:55 +0000

    test method to create mask.

--- models/test/pytorch_basics.py
+++ models/test/pytorch_basics.py
@@ -10,6 +10,8 @@ import numpy
 import torch
 from torch.nn import functional
 
+from models.utils.model_utils import get_masks
+
 torch.manual_seed(200)
 
 x = torch.rand(2, 3)
@@ -48,15 +50,21 @@ def buffered_mask(tensor):
         _mask = torch.triu(fill_with_neg_inf(_mask.resize_(dim, dim)), 1)
     return _mask[:dim, :dim]
 
+
 attn_weights += buffered_mask(attn_weights).unsqueeze(0)
-print(attn_weights)
+# print(attn_weights)
 
 attn_weights = torch.rand(4, 4, 4)
 b, h, w = attn_weights.size()
-print(attn_weights)
+# print(attn_weights)
 
 mask_diagonal = False
 maskval=float('-inf')
-indices = torch.triu_indices(h, w, offset=0 if mask_diagonal else 1)
-x = attn_weights[:, indices[0], indices[1]] = maskval
-print(attn_weights)
+# indices = torch.triu_indices(h, w, offset=0 if mask_diagonal else 1)
+# x = attn_weights[:, indices[0], indices[1]] = maskval
+# print(attn_weights)
+
+x = torch.Tensor([[1, 2, 4, 0], [3, 4, 0, 0], [1, 2, 23, 45]])
+lenghts = torch.Tensor([3, 2, 4])
+slen, bs = x.size()
+print(get_masks(slen, y, causal=True))
