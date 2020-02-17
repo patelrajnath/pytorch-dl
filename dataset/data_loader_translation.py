60e2479339e353ee1aca6000f8803fa44e0dcbac
Raj Nath Patel <patelrajnath@gmail.com>
2020-02-17 22:43:43 +0000

    Add sampler.

--- dataset/data_loader_translation.py
+++ dataset/data_loader_translation.py
@@ -10,6 +10,60 @@ import torch
 import tqdm
 from torch.utils.data import Dataset
 
+import numpy as np
+from random import shuffle
+from torch.utils.data.sampler import Sampler
+
+
+class BySequenceLengthSampler(Sampler):
+
+    def __init__(self, data_source,
+                 bucket_boundaries, batch_size=64, ):
+        super().__init__(data_source)
+        self.data_source = data_source
+        ind_n_len = []
+        for i, p in enumerate(data_source):
+            ind_n_len.append((i, p.shape[0]))
+        self.ind_n_len = ind_n_len
+        self.bucket_boundaries = bucket_boundaries
+        self.batch_size = batch_size
+
+    def __iter__(self):
+        data_buckets = dict()
+        # where p is the id number and seq_len is the length of this id number.
+        for p, seq_len in self.ind_n_len:
+            pid = self.element_to_bucket_id(p, seq_len)
+            if pid in data_buckets.keys():
+                data_buckets[pid].append(p)
+            else:
+                data_buckets[pid] = [p]
+
+        for k in data_buckets.keys():
+            data_buckets[k] = np.asarray(data_buckets[k])
+
+        iter_list = []
+        for k in data_buckets.keys():
+            np.random.shuffle(data_buckets[k])
+            iter_list += (np.array_split(data_buckets[k]
+                                         , int(data_buckets[k].shape[0] / self.batch_size)))
+        shuffle(iter_list)  # shuffle all the batches so they arent ordered by bucket
+        # size
+        for i in iter_list:
+            yield i.tolist()  # as it was stored in an array
+
+    def __len__(self):
+        return len(self.data_source)
+
+    def element_to_bucket_id(self, x, seq_length):
+        boundaries = list(self.bucket_boundaries)
+        buckets_min = [np.iinfo(np.int32).min] + boundaries
+        buckets_max = boundaries + [np.iinfo(np.int32).max]
+        conditions_c = np.logical_and(
+            np.less_equal(buckets_min, seq_length),
+            np.less(seq_length, buckets_max))
+        bucket_id = np.min(np.where(conditions_c))
+        return bucket_id
+
 
 class TranslationDataSet(Dataset):
     """
