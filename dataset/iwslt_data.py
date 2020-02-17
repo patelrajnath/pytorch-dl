f5cb8bc69e43a5dbef7b815ac1e553fa6a079bd6
Raj Nath Patel <patelrajnath@gmail.com>
2020-02-17 23:46:35 +0000

    update code to use the mask.

--- dataset/iwslt_data.py
+++ dataset/iwslt_data.py
@@ -144,7 +144,7 @@ class LabelSmoothing(nn.Module):
 
     def __init__(self, size, padding_idx, smoothing=0.0):
         super(LabelSmoothing, self).__init__()
-        self.criterion = nn.KLDivLoss(size_average=False)
+        self.criterion = nn.KLDivLoss(reduction='sum')
         self.padding_idx = padding_idx
         self.confidence = 1.0 - smoothing
         self.smoothing = smoothing
