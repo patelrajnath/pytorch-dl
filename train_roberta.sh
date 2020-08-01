d2c2d39d1812bd6320ab739a6fedc7a50c4fc93c
Raj Nath Patel <patelrajnath@gmail.com>
2020-08-01 19:55:21 +0100

    Add shell script to train roberta model.

--- train_roberta.sh
+++ train_roberta.sh
@@ -4,7 +4,6 @@ python pretrain_roberta.py \
 	-data roberta-data-bin/demo \
 	-save_model roberta-demo-model \
 	--batch_type tokens \
-	--enc_layers 1 \
-	--batch_size 1024 \
+	--batch_size 4096 \
 	--gpu_ranks 0 \
 	--single_pass
