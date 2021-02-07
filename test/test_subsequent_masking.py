d80bc376ac7d374ece34f5d6604794b286246818
Makeesy AI <makeesyai@gmail.com>
2021-02-07 16:44:27 +0000

    Add code to explain subsequent masking

--- test/test_subsequent_masking.py
+++ test/test_subsequent_masking.py
@@ -1,7 +1,13 @@
 import torch
-
-from dataset.iwslt_data import subsequent_mask
 from torch.autograd import Variable
+import numpy as np
+
+
+def subsequent_mask(size):
+    "Mask out subsequent positions."
+    attn_shape = (1, size, size)
+    subsequent_mask = np.triu(np.ones(attn_shape), k=1).astype('uint8')
+    return torch.from_numpy(subsequent_mask) == 0
 
 
 def make_std_mask(tgt, pad):
